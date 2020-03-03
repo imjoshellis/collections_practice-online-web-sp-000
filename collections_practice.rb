5865892239b3fa6a52ac295d3c4e7a7f42c76887
Josh Ellis <joshe523@gmail.com>
2020-03-03 04:07:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,3 +2,11 @@
 def sort_array_asc(arr)
   arr.sort { |a, b| a <=> b }
 end
+
+def sort_array_desc(arr)
+  arr.sort { |a, b| b <=> a }
+end
+
+def sort_array_char_count(arr)
+  arr.sort { |a, b| a.size <=> b.size }
+end
